933c27ab2a1ea81d6a5973e1e31770376760c140
JasonOwz <aaymecho3@gatech.edu>
2022-12-16 16:45:55 -0500

    bird addition+ format change

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
-import pygame
+import pygame, os, sys, math
 from pygame.locals import *
-import os
-import sys
-import math
 
 pygame.init()
 
-
+#-------------------------global variables----------------------------------------
 window_size = width, height = 1200, 450
 screen = pygame.display.set_mode(window_size)
 speed = 30
-
-bg = pygame.image.load(os.path.join('images','bg.png')).convert()
-bgX = 0
-bgX2 = bg.get_width()
 clock = pygame.time.Clock()
+
+#background globals
+bg = pygame.image.load(os.path.join('images','bg.png')).convert()
+bg_x = 0
+bg_x2 = bg.get_width()
+
+#bird globals
+bird = pygame.image.load(os.path.join('images', 'bird.png'))
+bird_x = width/3 - bird.get_width()/2
+bird_y = height/2 - bird.get_height()/2
+
+#game globals
+game_over = False
+
+
+#----------------------functions-------------------------------------------------
+
+#updates bird
+def update_bird():
+    global bird_x, bird_y
+    screen.blit(bird, (bird_x, bird_y))
+    if (pygame.key.get_pressed()[pygame.K_SPACE]): bird_y -= 8
+    else: bird_y += 5
+
+#updates and resets scrolling background
+def update_background():
+    global bg_x, bg_x2
+    bg_x -= 1.5
+    bg_x2 -= 1.5
+    if (bg_x < bg.get_width() * -1):
+        bg_x = bg.get_width()
+    if (bg_x2 < bg.get_width() * -1):
+        bg_x2 = bg.get_width()
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x2, 0))
+
 
 
 while True:
-    #allows user to close the application
     clock.tick(speed)
-    bgX -= 1.5
-    bgX2 -= 1.5
-
-    #updates and resets scrolling background
-    if (bgX < bg.get_width() * -1):
-        bgX = bg.get_width()
-    if (bgX2 < bg.get_width() * -1):
-        bgX2 = bg.get_width()
-        
-    screen.blit(bg, (bgX, 0))
-    screen.blit(bg, (bgX2, 0))
+    update_background()
+    update_bird()
     pygame.display.update()
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: sys.exit()
