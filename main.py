36a9a3dd4481f4f516560371fe04ba8495faf228
JasonOwz <aaymecho3@gatech.edu>
2022-12-17 14:01:36 -0500

    optimization + rework

--- main.py
+++ main.py
@@ -1,74 +1,40 @@
-import pygame, os, sys, math
+import pygame, os, random
 from pygame.locals import *
 
 pygame.init()
 
-#-------------------------global variables----------------------------------------
-window_size = width, height = 1200, 450
-screen = pygame.display.set_mode(window_size)
-speed = 30
 clock = pygame.time.Clock()
+clock_speed = 60
 
-#background globals
-bg = pygame.image.load(os.path.join('images','bg.png')).convert()
-bg_x = 0
-bg_x2 = bg.get_width()
-
-#bird globals
-bird = pygame.image.load(os.path.join('images', 'bird.png'))
-bird_x = width/3 - bird.get_width()/2
-bird_y = height/2 - bird.get_height()/2
-
-#game globals
-logo = pygame.image.load(os.path.join('images', 'logo.png'))
-play_button = pygame.image.load(os.path.join('images', 'playbtn.png'))
-start = True
-menu = False
-game_over = False
+screen_size = width, height = 864, 936
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('Flappy Bird')
 
 
-#----------------------functions-------------------------------------------------
+#vars for game
+floor_scroll = 0
+scroll_speed = 3
 
-#updates bird
-def update_bird():
-    global bird_x, bird_y
-    screen.blit(bird, (bird_x, bird_y))
-    if (bird_y <= 0 or bird_y >= height - bird.get_height()): game_over = True
-    elif (pygame.key.get_pressed()[pygame.K_SPACE]): bird_y -= 8
-    else: bird_y += 5
+#images loaded
+bg = pygame.image.load(os.path.join('images', 'bg.png'))
+floor = pygame.image.load(os.path.join('images', 'floor.png'))
 
+running = True
 
-#def update_pipe():
+while running:
+    clock.tick(clock_speed)
 
+    #draws background
+    screen.blit(bg, (0,0))
 
-#updates and resets scrolling background
-def update_background():
-    global bg_x, bg_x2
-    bg_x -= 1.5
-    bg_x2 -= 1.5
-    if (bg_x < bg.get_width() * -1):
-        bg_x = bg.get_width()
-    if (bg_x2 < bg.get_width() * -1):
-        bg_x2 = bg.get_width()
-    screen.blit(bg, (bg_x, 0))
-    screen.blit(bg, (bg_x2, 0))
+    #draws scrolling floor
+    screen.blit(floor, (floor_scroll, height))
+    floor_scroll -= scroll_speed
 
-#updates and checks if user closes applicaiton
-def check_close():
-    pygame.display.update()
+    if (abs(floor_scroll) > 34):
+        floor_scroll = 0
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-while ~game_over:
-    clock.tick(speed)
-    update_background()
-    update_bird()
-
-    #start menu until space is pressed
-    while (start):
-        screen.blit(logo, (width/2 - logo.get_width()/2, height/4 - logo.get_height()))
-        screen.blit(play_button, (width/2 - play_button.get_width()/2, height - 150))
-        if (pygame.key.get_pressed()[pygame.K_SPACE]): start = False;
-        check_close()
-
-    check_close()
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+pygame.quit()
